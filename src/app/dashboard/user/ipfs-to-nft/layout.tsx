717a61beb5026989e0945dc314ad72363e00d726
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-11-19 12:44:46 +0530

    removed unnecessary layout.tsx

--- src/app/dashboard/user/ipfs-to-nft/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-import React from "react";
-import { useActiveWallet } from "thirdweb/react";
-
-function IPFSToNFTLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
-}
-
-export default IPFSToNFTLayout;
