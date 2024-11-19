0f4fc8d2b41ca65238c92a1a567bc936377844af
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-11-19 12:45:24 +0530

    added base page for recent invoices

---
+++ src/app/dashboard/user/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UserInvoicesPage() {
+  return (
+    <div className="absolute inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+      UserInvoicesPage
+    </div>
+  );
+}
+
+export default UserInvoicesPage;
