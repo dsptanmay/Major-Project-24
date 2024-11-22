9337b9c574247497f2ba0e723ff56cb7cb0a98e5
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-11-22 19:24:57 +0530

    added environment file for ts autocompletion

---
+++ env.d.ts
@@ -0,0 +1,10 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_THIRDWEB_CLIENT_ID: string;
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
+    CLERK_SECRET_KEY: string;
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: string;
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: string;
+    NEXT_PUBLIC_CONTRACT_ADDRESS: string;
+  }
+}
