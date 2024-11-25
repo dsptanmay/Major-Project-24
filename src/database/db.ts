60fa8f48ae483df494b66fb2e8bf2b54f4cc0c71
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-11-25 22:43:58 +0530

    renamed variables; removed dbclient export

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,5 +2,5 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema";
 import postgres from "postgres";
 
-export const dbClient = postgres(process.env.DATABASE_URL, { prepare: false });
-export const db = drizzle({ client: dbClient, schema });
+const client = postgres(process.env.DATABASE_URL, { prepare: false });
+export const db = drizzle({ client: client, schema });
