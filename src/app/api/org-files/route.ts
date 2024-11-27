6bae02b3c0b55d20bc6e6688eb208a397f6424a3
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-11-27 18:09:44 +0530

    functionality to add title and description

--- src/app/api/org-files/route.ts
+++ src/app/api/org-files/route.ts
@@ -1,10 +1,13 @@
 import { db } from "@/database/db";
-import { organizationGrantedTokens } from "@/database/schema";
+import { organizationGrantedTokens, userNFTsTable } from "@/database/schema";
+import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
 interface OrganizationPostRequest {
   org_name: string;
   token_id: string;
+  title: string;
+  description: string;
 }
 
 export async function POST(request: NextRequest) {
@@ -17,9 +20,22 @@ export async function POST(request: NextRequest) {
         { error: "Missing required fields" },
         { status: 404 }
       );
+
+    const userData = await db
+      .select()
+      .from(userNFTsTable)
+      .where(eq(userNFTsTable.token_id, token_id));
+
+    if (userData.length === 0)
+      return NextResponse.json(
+        { error: "Token does not exist" },
+        { status: 400 }
+      );
+    const title = userData[0].title;
+    const description = userData[0].description;
     const newRecord = await db
       .insert(organizationGrantedTokens)
-      .values({ org_name, token_id })
+      .values({ org_name, token_id, title, description })
       .returning();
 
     if (newRecord.length === 0)
