3253a6b3c1b9934b80485f416d1e24bf0ca644eb
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-02 14:26:06 +0530

    added DELETE request

--- src/app/api/access/route.ts
+++ src/app/api/access/route.ts
@@ -1,10 +1,11 @@
 import { db } from "@/database/db";
 import {
+  notificationsTable,
   organizationGrantedTokens,
   organizationWalletTable,
   userNFTsTable,
 } from "@/database/schema";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
@@ -27,7 +28,7 @@ export async function GET(request: NextRequest) {
         organization_name: organizationGrantedTokens.org_name,
         organization_address: organizationWalletTable.wallet_address,
         title: organizationGrantedTokens.title,
-        token_id: organizationGrantedTokens.token_id,
+        nft_token_id: organizationGrantedTokens.token_id,
       })
       .from(organizationGrantedTokens)
       .innerJoin(
@@ -50,6 +51,53 @@ export async function GET(request: NextRequest) {
       );
 
     return NextResponse.json(userTokens, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+  try {
+    const searchParams = request.nextUrl.searchParams;
+    const organization_name = searchParams.get("orgName");
+    const nft_token_id = searchParams.get("tokenId");
+    if (!organization_name || !nft_token_id)
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
+
+    const deletedRecords = await db
+      .delete(organizationGrantedTokens)
+      .where(
+        and(
+          eq(organizationGrantedTokens.token_id, nft_token_id),
+          eq(organizationGrantedTokens.org_name, organization_name)
+        )
+      )
+      .returning();
+
+    const deletedNotifications = await db
+      .delete(notificationsTable)
+      .where(
+        and(
+          eq(notificationsTable.nft_token_id, nft_token_id),
+          eq(notificationsTable.org_name, organization_name)
+        )
+      )
+      .returning();
+
+    if (deletedNotifications.length === 0 || deletedRecords.length === 0)
+      return NextResponse.json({ error: "Missing token ID" }, { status: 404 });
+
+    return NextResponse.json(
+      { message: "Deleted records successfully" },
+      { status: 200 }
+    );
   } catch (error) {
     console.error(error);
     return NextResponse.json(
